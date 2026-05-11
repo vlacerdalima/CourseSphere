a8caf3fa176c4d2b87fcc0e8bea516461a77b19f
Vitor Lacerda <vlacerdalima@gmail.com>
2026-05-11 09:43:17 -0300

    fix: wrong analytics import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { Toaster } from "@/components/ui/sonner";
 
 import { AuthProvider } from "@/contexts/AuthContext";
